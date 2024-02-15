9be57c20e973a8d0c486b12b240e638b00762c0a
viviane <vivianebinet@hotmail.com>
2024-02-15 17:12:36 -0500

    Q3 ACM

--- Q3_ACM/acm.py
+++ Q3_ACM/acm.py
@@ -12,15 +12,39 @@ INFINITY = math.inf
 #add a return value, but don't use other librairies
 
 
-print("test ")
 
 def read_problems(input_file):
     # lecture du fichier/file reading
     file = open(input_file,"r")
     lines = file.readlines()
     file.close()
-    print("read")
-    print(lines)
+
+    numberOfGraphs = int(lines[0].replace("\n", ""))
+    Graphs = [[0]]*numberOfGraphs
+    line = 1
+    for i in range(numberOfGraphs):
+        
+        nbVertices = int(lines[line])
+        Vertices = [0]*nbVertices       # initialize array
+
+
+        for j in range (nbVertices):
+            coord = lines[line+1+j].split(" ")
+
+            x = float(coord[0])
+            y = float(coord[1].replace("\n", ""))
+            Vertices[j] = [x, y]
+
+        line += nbVertices +1
+        Graphs[i] = Vertices
+
+
+    return Graphs
+
+        
+    
+        
+
 
 def write(fileName, content):
     #écrire la sortie dans un fichier/write output in file
@@ -29,14 +53,65 @@ def write(fileName, content):
     file.close()
 
 
+#Distance entre sommet u et sommet v
+def distance(u, v):
+    return math.sqrt((u[0]-v[0])**2 + (u[1]-v[1])**2)
+
+#Trouver la distance minimum
+def findMin(dist, vertToAdd):
+    min = vertToAdd[0]
+    for u in vertToAdd:
+        d = dist[u]
+        if d < dist[min]:
+            min = u
+    return min
+
+
 #Fonction main/Main function
 def main(args):
     input_file = args[0]
     output_file = args[1]
-    read_problems(input_file)
-    #TODO : Continuer ici/Complete here...
-    #Vous pouvez découper votre code en d'autres fonctions...
-    #You may split your code in other functions...
+    Graphs = read_problems(input_file)
+
+    toWrite = ""
+
+    for g in Graphs:
+
+        vertices = g
+        dist = [INFINITY]*len(vertices)     # Distances de chaque sommet au graphe en cours
+        vertToAdd = list(range(0, len(vertices)))  # Sommets qui restent a ajouter a la solution
+        vertToAdd.pop(0)
+        
+        firstVertex = vertices[0]
+        dist[0] = 0
+        v = firstVertex
+        for u in vertToAdd:
+            dist[u] = distance(v, vertices[u])
+
+
+        totalDist = 0 
+
+        while (len(vertToAdd) > 0 ):
+            min = findMin(dist, vertToAdd)
+
+            vertToAdd.remove(min)
+            totalDist += dist[min]
+
+
+            # Mettre a jour les distances
+            for u in vertToAdd:
+                d = distance(vertices[u], vertices[min])
+                if d < dist[u]:
+                    dist[u] = d
+            
+        toWrite += str(totalDist) + "\n"
+
+    write(output_file, toWrite)
+
+
+
+
+
 
 
 # TODO
